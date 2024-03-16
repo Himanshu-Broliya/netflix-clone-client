c5e076ef6125330dcbf385fafb31d35ac5a8f39c
Himanshu <himanshubroliya2002@gmail.com>
2024-03-16 16:22:17 +0530

    changes

---
+++ src/constant.js
@@ -0,0 +1,7 @@
+export const option = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYTdiNmFlYmM0ODY3NmM4M2ExMmE2N2QyMWI2MGU3YSIsInN1YiI6IjY0ZGZhOTAzMzcxMDk3MDBmZmI5YzY4NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VatADtWBXlLQ28c2AGjG8jIR0yeo31xrb2i-6BwqGWs'
+    }
+  };
